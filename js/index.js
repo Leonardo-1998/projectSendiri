5fc4ba55ff110aa92e0a6db14789ceeb9f8af6cc
leonardo-1998 <leonardo.agst98@gmail.com>
2025-03-01 22:43:07 +0700

    Feat : Penambahan fitur input angka, decimal, dan negatif

--- js/index.js
+++ js/index.js
@@ -1,33 +1,70 @@
-function input (press){
-    // document.getElementById(press)
-    console.log(press)
-    return press
+// menerima input angka lalu menunjukannya
+function input(input) {
+    // console.log(input)
+    tempStr += input
+    currentNumber = Number(tempStr)
+    // console.log(currentNumber)
+    document.getElementById("currentNumber").innerHTML = currentNumber
 }
 
-function operator (press) {
-    console.log(press)
-    return press
+function operator(operator) {
+    console.log(operator)
+    console.log(tempStr)
+    console.log(currentNumber)
+    calculation.push(Number(tempStr))
+    // return press
 }
 
-function deleteAll(){
+function deleteAll() {
     console.log("deleteAll")
     // return clear
 }
 
-function negative(){
+function negative() {
     console.log("negative")
+    if (tempStr[0] !== "-") {
+        tempStr = "-" + tempStr
+        currentNumber = Number(tempStr)
+        document.getElementById("currentNumber").innerHTML = currentNumber
+    } else {
+        tempStr = tempStr.slice(1)
+        console.log(tempStr)
+        currentNumber = Number(tempStr)
+        document.getElementById("currentNumber").innerHTML = currentNumber
+    }
     // return a
 }
 
-function deleteOne(){
-    console.log("deleteOne")
-    // return 
+function deleteOne() {
+    // console.log("deleteOne")
+    tempStr = tempStr.slice(0, tempStr.length - 1)
+    // console.log(tempStr[tempStr.length-1])
+    if(tempStr[tempStr.length-1] === "."){
+        // console.log("a")
+        tempStr = tempStr.slice(0, tempStr.length - 1)
+    }
+    // console.log(tempStr)
+    currentNumber = Number(tempStr)
+    document.getElementById("currentNumber").innerHTML = currentNumber
 }
 
-function decimal(){
-    console.log("decimal")
+function decimal() {
+    if (tempStr.indexOf(".") === -1) {
+        console.log("decimal")
+        tempStr += "."
+        console.log(tempStr)
+    }
+    // currentNumber = Number(tempStr)
+    // document.getElementById("currentNumber").innerHTML = currentNumber
 }
 
-function equal(){
+function equal() {
     console.log("equal")
 }
+
+let tempStr = ""
+let calculation = []
+let currentNumber = 0
+
+document.getElementById("currentNumber").innerHTML = BigInt(currentNumber)
+document.getElementById("currentOperation").innerHTML = currentNumber

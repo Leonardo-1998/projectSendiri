e2af77b02c59bb3a9c5ce66576a3e34db9d42058
leonardo-1998 <leonardo.agst98@gmail.com>
2025-03-01 21:29:51 +0700

    Feat : Penambahan function kosong pada setiap tombol

---
+++ js/index.js
@@ -0,0 +1,33 @@
+function input (press){
+    // document.getElementById(press)
+    console.log(press)
+    return press
+}
+
+function operator (press) {
+    console.log(press)
+    return press
+}
+
+function deleteAll(){
+    console.log("deleteAll")
+    // return clear
+}
+
+function negative(){
+    console.log("negative")
+    // return a
+}
+
+function deleteOne(){
+    console.log("deleteOne")
+    // return 
+}
+
+function decimal(){
+    console.log("decimal")
+}
+
+function equal(){
+    console.log("equal")
+}
